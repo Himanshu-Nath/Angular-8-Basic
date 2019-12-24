4933f12d1eff821e82555677db37e2f3d2f4c63a
himanshu-nath <hnath723@gmail.com>
2019-12-24 13:42:56 +0530

    first commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'iPhone 11 pro',
+    price: 100799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'iPhone 11',
+    price: 90699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'iPhone X',
+    price: 60299,
+    description: 'Best in segment'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
